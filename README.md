6e357061abaa85fc1f309e256f8c2be5b20cb5c0
gevin <flyhigher139@gmail.com>
2021-12-01 15:50:59 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# spring-oauth-in-pratice
+# spring-oauth-in-practice
+
+基于目前出于deprecated状态的spring security oauth，实现oauth相关应用
---
+++ auth-server-demo/src/main/java/com/gevinzone/authserver/demo/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.gevinzone.authserver.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().passwordEncoder(passwordEncoder())
+                .withUser("admin").password("admin").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/oauth/**").permitAll()
+                .and().formLogin().permitAll()
+                .and().logout().permitAll();
+    }
+}
