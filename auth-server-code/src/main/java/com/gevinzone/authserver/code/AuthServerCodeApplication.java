3324511b2017083c27ebc937d6fb33b0b79dc9a0
gevin <flyhigher139@gmail.com>
2021-12-03 15:42:31 +0800

    auth jdbc

---
+++ auth-server-code/src/main/java/com/gevinzone/authserver/code/AuthServerCodeApplication.java
@@ -0,0 +1,11 @@
+package com.gevinzone.authserver.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerCodeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerCodeApplication.class, args);
+    }
+}
---
+++ auth-server-code/src/main/java/com/gevinzone/authserver/code/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.gevinzone.authserver.code.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().passwordEncoder(passwordEncoder())
+                .withUser("admin").password("admin").roles("ADMIN");
+    }
+}
